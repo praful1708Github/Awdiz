6ea47cf2c65afd0f3f8e8c6d0ab5014f06515ca4
praful1708Github <123794402+praful1708Github@users.noreply.github.com>
2023-01-31 10:58:20 +0530

    first page started

---
+++ javascript.js
@@ -0,0 +1,9 @@
+var data = 12;
+console.log("data:",data);
+console.log("data type:",typeof (data));
+var data2 = '12';
+console.log("data:",data2);
+console.log("data type :",typeof(data2));
+var data3 = false;
+console.log("data:",data3);
+console.log("data type:",typeof(data3));
